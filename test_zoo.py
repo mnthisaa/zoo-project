4d767685b1f0f42d396eca766419532154252459
mnthisaa <mnthisakcn@gmail.com>
2024-11-11 05:36:59 +0000

    6687068

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,47 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(1), 50) 
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)           
        
     # Add your additional test cases here.
+    def test_c1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+    def test_c1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+    def test_c1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)  
+    def test_c1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+    def test_c1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)            
+
+    def test_c2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(25), 150)
+    def test_c2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    def test_c2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)  
+    def test_c2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+    def test_c2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)            
+
+    def test_c3_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(65), 100) 
+    def test_c3_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)    
+
+    def test_c4_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-2),"Invalid age" )  
+    def test_c4_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1),"Invalid age" )        
 
 if __name__ == '__main__':
     unittest.main()
